300ae019f73aea576d740d2de791e5fe75cb901a
Robert <galindo.rob@gmail.com>
2018-02-10 20:18:28 -0700

    Modified gulp file.

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+		.on('error', function() {
+			console.log(errorInfo, toString());
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('cssInject', ['styles'], function() {
+	gulp.src('./app/temp/styles/styles.css')
+		.pipe(browserSync.stream());
+});
